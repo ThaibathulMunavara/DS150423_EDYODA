17d1f75c5f1173f0c6617ae6b8344bd6b3bbbe33
ThaibathulMunavara <132924750+ThaibathulMunavara@users.noreply.github.com>
2023-05-09 01:26:01 +0530

    operator

--- Python/_2_operator/_0_input_function.py
+++ Python/_2_operator/_0_input_function.py
@@ -3,5 +3,17 @@
 no1 = int(input("Enter your no1 : "))
 no2 = int(input("Enter your no2 : "))
 
-add = no1 + no2
-print(add)
+#add = no1 + no2
+#print(add)
+
+eq= no1 == no2
+print("Equal",eq)
+
+greater = no1>=no2
+print("Greater:",greater)
+
+
+# operation classifies into 2 type:
+# operand( eg: 2,3) and operator(eg:+,-)
+# operator classifies into 4  : ARTHEMATIC OPERATOR , COMPARISON OPERATOR , LOGICAL OPERATOR ,MEMBERSHIP OPERATOR
+#                               IDENTITY OPERATOR
